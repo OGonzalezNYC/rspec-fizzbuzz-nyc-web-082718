6832efa7a894d4b8f25886a97165dcdf3650174a
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-08 22:24:39 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,10 +8,4 @@ def fizzbuzz(number)
   else
     nil 
   end 
-end 
-
-
-Test Driving FizzBuzz
-A classic programming problem is FizzBuzz. It is considered the Stairway to Heaven of programming because there are so many different ways to play it and everyone plays it.
-
-The goal of FizzBuzz is to build a program that can take a number and if the number is evenly divisible by 3, it should return "Fizz", if it's divisible by 5, it should return "Buzz", and if it's divisible by both 3 and 5, it should return "FizzBuzz".
+end
